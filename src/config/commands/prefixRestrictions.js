a3e2b6e54162e887d964b257df75950a16aface9
whayok <deeskiqq@gmail.com>
2026-08-17 00:45:10 -0400

    Update prefixRestrictions.js

--- src/config/commands/prefixRestrictions.js
+++ src/config/commands/prefixRestrictions.js
@@ -79,7 +79,7 @@ export function getPrefixRestriction(command, args, resolveSubcommandAlias) {
   const commandJson = command.data.toJSON();
   const commandName = commandJson.name?.toLowerCase();
 
-  if (command.prefixOnly === false || command.CommaOnly === true) {
+  if (command.prefixOnly === false || command.slashOnly === true) {
     return { blocked: true, reason: 'This command is only available as a slash command.' };
   }
 
