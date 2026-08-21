aa793653dce1b61e0e56113f18d91a587736e365
whayok <deeskiqq@gmail.com>
2026-08-20 22:01:02 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "High", // required by Discord API, not shown in the client
+        name: "hELLO", // required by Discord API, not shown in the client
         state: "",     // this is what people actually see
         type: 4,               // Custom
       },
